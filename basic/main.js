59c8613214c6b89737b285b22d3f2d6c70538392
Sadique Ahmed <sadiqueahmed40@gmail.com>
2024-09-07 03:03:49 -0700

    Initial commit

---
+++ basic/main.js
@@ -0,0 +1,14 @@
+import * as THREE  from 'three';
+
+
+// 1. Create the scene
+
+// 2. Add the camera
+
+// 3. Create and add the cube object
+
+// 4. Add lighting
+
+// 5. Set up the renderer
+
+// 6. Animate the scene
