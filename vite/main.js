83cad9ad5282fb28de80b422891e12b1a201fb1b
Sadique Ahmed <sadiqueahmed40@gmail.com>
2024-09-07 09:14:47 -0700

    Update main.js

--- vite/main.js
+++ vite/main.js
@@ -4,3 +4,13 @@ const canvas = document.getElementById('canvas');
 
 // 1. Scene
     const scene = new THREE.Scene();
+    scene.background = new THREE.Color('#F0F0F0');
+
+// 2. Camera
+    const camera = new THREE.PerspectiveCamera(fov: 75, aspect: window.innerWidth / window.innerHeight, near:0.1, far: 1000);
+    camera.position.z = 5;
+
+// 3. Object
+    const geometry = new THREE.DodechedronGeometry();
+    const material = new THREE.MeshBasicMaterial({color: '#F468585',  emissive: '#468585'});
+    const dodecahedron = new THREE.Mesh(geometry, material);
