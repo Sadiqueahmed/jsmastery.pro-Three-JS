a449e030e4fbb57cd358df74c56ee9776e0f4334
Sadique Ahmed <sadiqueahmed40@gmail.com>
2024-09-07 04:11:23 -0700

    Update main.js

---
+++ basic/main.js
@@ -0,0 +1,14 @@
+import * as THREE  as 'three';
+
+// 1. Create the scene
+    const scene : THREE.scene = new THREE.scene();
+    scene.background = new THREE.Color('#F0F0F0');
+// 2. Add the camera
+    const camera = new THREE.PerspectiveCamera();
+// 3. Create and add a cube object
+
+// 4. Add lightning
+
+// 5. Set up the renderer
+
+// 6. Animate the scene
