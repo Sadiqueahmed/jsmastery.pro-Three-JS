2b3092b42ae1712eced261cfade228dd41464390
Sadique Ahmed <sadiqueahmed40@gmail.com>
2024-09-07 08:58:45 -0700

    .

--- basic/main.js
+++ basic/main.js
@@ -4,7 +4,7 @@ import * as THREE from 'three' ;
     const scene = new THREE.Scene();
     scene.background = new THREE.Color('#F0F0F0');
 // 2. Add the camera
-    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000    );
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.01, 1000    );
     camera.position.z = 5;
 // 3. Create and add a cube object
     const geometry =  new THREE.BoxGeometry();
